8390e4d83101cce5d3501c5e8a2eeca168471d1d
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 01:45:54 +0530

    global- variables

---
+++ C_PROGRAMMING/C_PROGRAMS/Operator/p3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int x=10;  // Global variables
+int y;  // Global variable
+
+
+void office()
+{
+  printf("\n In office Nusta Coding....\n");
+}
+void main ()
+{
+   int x=10; // local variable
+   printf("\n In Main function");
+   office();
+  printf("Sleep\n");
+
+}
