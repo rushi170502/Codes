73dff3e5ba89cd4e866771894816bf5ae24c7fdb
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 15:15:10 +0530

    WAP to print all Armstrong number in given range by the user

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/MAJIC_SESSIONS/Majic_session_2/program10.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+
+class Demo{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter the Lower limit  :  ");
+        int start = Integer.parseInt(br.readLine());
+        System.out.println("Enter the Higher limit  :  ");
+        int end = Integer.parseInt(br.readLine());
+
+        for(int i = start; i <= end ;i++){
+            int sum = 0;
+            int num = i;
+            int count = 0;
+            while(num != 0) {
+                count++;
+                num /= 10;
+              }
+            num = i;
+            while(num != 0){
+                int mul = 1;
+                int rem = num%10;
+                for(int j = 1; j <= count; j++){
+                    mul = mul * rem;
+                }
+                sum = sum + mul;
+                num /= 10;
+            }
+            if(sum == i)
+                System.out.print("\t"+i);
+        }
+    }
+}
