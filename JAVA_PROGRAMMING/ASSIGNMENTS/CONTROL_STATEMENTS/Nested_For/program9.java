345c23935fc0156d5c6eab97e30817c8e1fa1b48
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:25:07 +0530

    Simple pattern 9

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/Nested_For/program9.java
@@ -0,0 +1,26 @@
+
+// WAP to print following pattern
+//
+// 		 C    B    A
+// 		 C    B    A  
+// 		 C    B    A
+//
+
+class pattern{
+
+	public static void main(String[] args){
+		
+		for(int i= 1; i <= 3;i++){
+
+			char ch ='C';
+
+			for(int j = 1; j <= 3;j++){
+
+				System.out.print(ch+"   ");
+				ch--;
+			}
+			System.out.println();
+
+		}
+	}
+}
