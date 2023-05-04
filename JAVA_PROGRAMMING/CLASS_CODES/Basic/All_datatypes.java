078bbe57f49ad7bbd9d61282affe8dc9e19dd87b
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 15:37:46 +0530

    All datatypes check

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Basic/All_datatypes.java
@@ -0,0 +1,28 @@
+
+// ALL data types use in java..
+//
+// 1. int : 4 bytes
+// 2. float : 4 bytes
+// 3. char  : 2 bytes
+// 4: double : 8 bytes -for float
+// 5: long : 8 bytes - for integer
+//
+class Demo{
+
+	public static void main(String[] args){
+
+		int jer_No = 18;
+		float avg = 56.7f;// for float there f is compulsary..
+		char ch = 'A';
+		double revenue = 23.234554;
+		long  money = 348765423;
+
+		System.out.println(jer_No);
+		System.out.println(avg);
+		System.out.println(ch);
+		System.out.println(revenue);
+		System.out.println(money);
+
+	}
+
+}
