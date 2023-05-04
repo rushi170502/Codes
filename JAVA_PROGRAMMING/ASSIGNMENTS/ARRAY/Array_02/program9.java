222103df91dcb53eb9fdd8d086ebc5afccd1596b
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 15:32:50 +0530

    WAP to merge two arrays

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/ARRAY/Array_02/program9.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+
+class ArrayDemo{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter the size of array 1 : ");
+        int size1 = Integer.parseInt(br.readLine());
+
+        int arr1[] = new int[size1 ];
+        System.out.println("Enter the Array 1 elements  :");
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = Integer.parseInt(br.readLine());
+        }
+
+        System.out.println("Enter the size of array 2 : ");
+        int size2 = Integer.parseInt(br.readLine());
+        int arr2[] = new int[size2];
+
+        System.out.println("Enter the Array 2 elements  :");
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = Integer.parseInt(br.readLine());
+        }
+
+        int arr3[] = new int[size1+size2];
+        for(int i = 0; i < arr1.length; i++) {
+            arr3[i] = arr1[i];
+        }
+        for(int i = 0; i < arr2.length; i++) {
+            arr3[size1 + i] = arr2[i];
+        }
+
+        for(int i = 0 ; i< arr3.length ; i++)
+            System.out.print("\t"+arr3[i]);
+
+    }
+}
