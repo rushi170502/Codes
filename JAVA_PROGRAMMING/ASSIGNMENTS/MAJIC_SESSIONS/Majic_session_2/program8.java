647ce2e113a2a47efaf6b4c3824e4154be92b414
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 15:13:32 +0530

    WAP to print all palindrome between the given range by user..

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/MAJIC_SESSIONS/Majic_session_2/program8.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+class Demo {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the Lower limit  :  ");
+        int start = Integer.parseInt(br.readLine());
+        System.out.println("Enter the Higher limit  :  ");
+        int end = Integer.parseInt(br.readLine());
+
+        for(int i = start; i <= end ; i++){
+            int rev = 0;
+            int num = i;
+            while(num != 0) {
+                int rem = num % 10;
+                rev = rev * 10 + rem;
+                num /= 10;
+            }
+            if(rev == i)
+                System.out.print("\t"+i);
+        }
+    }
+}
