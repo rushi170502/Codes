b074b499de33317670408812a6c01b2a6aa65661
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 13:55:43 +0530

    Greater >> & less than operator <<

---
+++ C_PROGRAMMING/C_PROGRAMS/Statements/0_Basics/program3.c
@@ -0,0 +1,19 @@
+
+// WAP to find max number among 2 numbers. Take all the values from users...
+//
+#include<stdio.h>
+void main()
+{
+   int num1,num2;
+   printf("Enter the number1 and number2:\n");
+   scanf("%d",&num1);
+   scanf("%d",&num2);
+
+   if(num1 > num2){
+      printf("\n %d ",num1);
+   }
+   else{
+     printf("\n %d\n",num2);
+   }
+
+}   
