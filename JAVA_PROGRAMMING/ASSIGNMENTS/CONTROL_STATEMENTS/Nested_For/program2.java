b06b48c0e00e81411feb9ea824bceb31da23b1e4
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:21:50 +0530

    Simple pattern 1

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/Nested_For/program2.java
@@ -0,0 +1,24 @@
+
+// WAP to print following pattern
+//
+// 		4   5   6   7
+// 		4   5   6   7
+// 		4   5   6   7
+// 		4   5   6   7
+
+
+class pattern{
+
+	public static void main(String[] args){
+
+		for(int i= 1; i <= 4;i++){
+
+			for(int j = 1; j <= 4;j++){
+
+				System.out.print(j+3+"   ");
+			}
+			System.out.println();
+
+		}
+	}
+}
