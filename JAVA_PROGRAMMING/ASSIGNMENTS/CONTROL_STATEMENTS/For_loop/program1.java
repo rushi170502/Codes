83e4c97c61544422d6c70fc86a0565565c6d6c2d
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:12:31 +0530

    WAP to print the 1 to 100

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/For_loop/program1.java
@@ -0,0 +1,15 @@
+
+// WAP to print the 1 to 100
+// using for loop
+
+class Demo{
+
+	public static void main(String[] args){
+
+		int num = 100;
+		for(int i = 1; i <= num; i++){
+			System.out.println(i);
+		}
+	}
+
+}
