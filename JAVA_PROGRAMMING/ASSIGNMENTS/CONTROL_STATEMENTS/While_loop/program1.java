0717528969ba67422dd4638063ccf32d9253a056
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:43:03 +0530

    WAP to print the table of given number using while loop

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/While_loop/program1.java
@@ -0,0 +1,19 @@
+
+// WAP to print the table of given number using while loop
+//
+ 
+class While{
+
+
+	public static void main(String[] args){
+
+		int num = 2;
+		int i = 1;
+
+		while(i<=10){
+
+			System.out.println(num +" * "+i+ " = "+num*i);
+			i++;
+		}
+	}
+}
