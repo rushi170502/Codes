a10efacae2d4caa7b6ea1fbc6aa7e5e95179aeab
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:24:49 +0530

    Simple pattern 8

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/Nested_For/program8.java
@@ -0,0 +1,26 @@
+
+// WAP to print following pattern
+
+//    		d    d    d    d
+//    		c    c    c    c
+//    		b    b    b    b
+//    		a    a    a    a
+
+
+class pattern{
+
+	public static void main(String[] args){
+		
+		char ch = 'd';
+		for(int i= 1; i <= 4;i++){
+
+			for(int j = 1; j <= 4;j++){
+
+				System.out.print(ch+"   ");
+			}
+			ch--;
+			System.out.println();
+
+		}
+	}
+}
