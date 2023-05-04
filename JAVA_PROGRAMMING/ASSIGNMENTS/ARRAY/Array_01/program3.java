e4d42896caf0038742f9ec20138a83ac74457f10
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 15:22:01 +0530

    WAP to take size of array from user and also take integer elements from user print product of odd index elements only

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/ARRAY/Array_01/program3.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+
+class ArrayDemo{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+
+        System.out.println("Enter the size of array : ");
+        int size = Integer.parseInt(br.readLine());
+
+        int arr[] = new int[size];
+        int product = 1;
+        System.out.println("Enter the Array elements  :");
+        for(int i = 0; i < arr.length ; i++){
+            arr[i]= Integer.parseInt(br.readLine());
+            if(i % 2 == 1)
+                product = product * arr[i];
+
+        }
+        for(int i = 0; i< arr.length;i++)
+            System.out.print("\t"+arr[i]);
+
+        System.out.println();
+        System.out.println("\t"+"Product = "+product);
+    }
+}
