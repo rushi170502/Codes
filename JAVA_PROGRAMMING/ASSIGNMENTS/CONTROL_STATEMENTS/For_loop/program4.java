2a8d0b748740bf65c4860671c2ade8563c280112
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:16:09 +0530

    WAP to factors of given number

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/For_loop/program4.java
@@ -0,0 +1,19 @@
+
+// WAP to print the factors of  numbers between range
+//
+
+class Demo{
+
+	public static void main(String[] args){
+
+		int num = 100;
+		
+		for(int i = 1; i<= num; i++){
+
+			if(num % i == 0){
+
+				System.out.println(i);
+			}
+		}
+	}
+}
