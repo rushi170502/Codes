83d0188083296e0e4ce83da7c98d3c78cd3f3037
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:43:56 +0530

    WAP to find factorial of given number using while loop

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/While_loop/program2.java
@@ -0,0 +1,18 @@
+// WAP to find the factorial of given number..
+//
+class While{
+
+	public static void main(String[] args){
+
+		int num = 6;
+		int fact=1;
+		int i=1;
+		while(i <= num){
+
+			fact= fact*i;
+			i++;
+		}
+
+		System.out.println("Factorial is : "+fact);
+	}
+}
