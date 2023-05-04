7d0d5cf129cc768ba8d3659e6503dc3e6d0145e7
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:40:38 +0530

    Medium level Pattern 2

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/Pattern/Assignment_3/program2.java
@@ -0,0 +1,14 @@
+
+class pattern1 {
+    public static void main(String[] args) {
+        int row = 5;
+        int num = 1;
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num++ + "\t");
+            }
+            num++;
+            System.out.println();
+        }
+    }
+}
