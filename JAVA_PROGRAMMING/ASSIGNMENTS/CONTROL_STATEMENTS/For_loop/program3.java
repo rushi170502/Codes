0141e2f684c7f7a9acdfaa92bab561ca6bdfbe07
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:14:56 +0530

    WAP to print odd numbers in range

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/For_loop/program3.java
@@ -0,0 +1,15 @@
+
+// WAP to print the odd numbers between range..
+//
+
+class Demo {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 100; i++) {
+			if (i % 2 == 1)
+				System.out.println(i);
+
+		}
+	}
+}
