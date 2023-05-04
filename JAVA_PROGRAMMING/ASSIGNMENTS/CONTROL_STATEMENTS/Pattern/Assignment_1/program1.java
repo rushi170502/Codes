20248c8e4c61eeb7899afc06664e6bd942b3efbd
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:29:47 +0530

    Pattern 1

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/Pattern/Assignment_1/program1.java
@@ -0,0 +1,22 @@
+
+// WAP to print following pattern
+//
+// 	C2W1    C2W2    C2W3
+// 	C2W1    C2W2    C2W3
+// 	C2W1    C2W2    C2W3
+
+
+class pattern{
+
+	public static void main(String[] args){
+
+		for(int i = 1; i <= 3;i++){
+
+			for(int j = 1; j <= 3; j++){
+
+				System.out.print("\tC2W"+j);
+			}
+		System.out.println();
+		}
+	}
+}
