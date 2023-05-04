413e4ecfc628d78b5eb9fe6e9ab525c53f0b2706
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-04 14:49:17 +0530

    WAP rpint countdown of days

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/CONTROL_STATEMENTS/While_loop/program8.java
@@ -0,0 +1,19 @@
+
+// WAP to print the countdown of days.
+//
+ 
+class While{
+
+	public static void main(String[] args){
+
+		int days = 4;
+
+		while(days != 0){
+
+			System.out.println(days +" days remainings");
+			days--;
+		}
+
+		System.out.println(days +" days Assignment is overdue");
+	}
+}
