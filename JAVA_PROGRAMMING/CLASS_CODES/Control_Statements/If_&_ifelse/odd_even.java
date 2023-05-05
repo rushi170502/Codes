d493d5e8c1b20164ad9bcda2ba1e05d0c003c375
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:37:19 +0530

    WAP to number is odd or even

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Control_Statements/If_&_ifelse/odd_even.java
@@ -0,0 +1,16 @@
+
+// WAP to find the given number is odd or even...
+//
+ 
+class Demo{
+	
+	public static void main(String[] args){
+
+		int num1 = 23;
+
+		if(num1 % 2 == 0)
+			System.out.println(num1 +" is even number");
+		else
+			System.out.println(num1+ " is odd number");
+	}
+}
