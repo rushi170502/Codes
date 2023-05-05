3977884669c9441f6f99a5836ebb0460e1157f86
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 21:40:36 +0530

    concept of identity HashCode of various string

---
+++ JAVA_PROGRAMMING/CLASS_CODES/String/string/program5.java
@@ -0,0 +1,16 @@
+package string;
+
+
+class Demo{
+    public static void main(String[] args) {
+        
+        String str1 = "Rushi";
+        String str2 = str1;
+
+        String str3 = new String(str2);
+
+        System.out.println(System.identityHashCode(str1));
+        System.out.println(System.identityHashCode(str2));
+        System.out.println(System.identityHashCode(str3));
+    }
+}
