c397cf4fac2165ccfb9c7a22014ac56ae49b2c02
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:53:40 +0530

    WAP to print odd numbers from 1 to 20 by using while loop

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Control_Statements/While_loop/program3.java
@@ -0,0 +1,21 @@
+
+// WAP to print odd numbers upto 20;
+
+class Demo{
+
+	public static void main(String[] args){
+		
+		int num = 20;
+		int i = 1;
+
+		while(i<=num){
+
+			if(i % 2 == 1)
+				System.out.println(i);
+
+		i++;	
+
+
+		}
+	}
+}
