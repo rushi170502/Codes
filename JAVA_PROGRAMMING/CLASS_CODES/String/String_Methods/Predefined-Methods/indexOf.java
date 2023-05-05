300d349e99633ae0ae345218f7b64489c7d8dd50
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-06 00:19:54 +0530

    return the index of first instance of given character in a string

---
+++ JAVA_PROGRAMMING/CLASS_CODES/String/String_Methods/Predefined-Methods/indexOf.java
@@ -0,0 +1,15 @@
+
+// indexOf() method :
+import java.util.*;
+
+class MethodDemo{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the string");
+    String str1 = sc.nextLine();
+   
+    // method uses
+    System.out.println("Index is : "+str1.indexOf('s', 0));
+
+    }
+}
