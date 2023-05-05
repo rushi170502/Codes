e5329340f4d0ef60a4df2112990f1a1ca98ab4a7
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:21:14 +0530

    Arithmetic operators

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Operators/Arithmmetic.java
@@ -0,0 +1,19 @@
+
+// WAP to ue all arithmetic opeators...
+//
+class Operators{
+
+	public static void main( String args[]){
+
+		int x = 10;
+		int y = 20;
+
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+
+		
+	}
+}
