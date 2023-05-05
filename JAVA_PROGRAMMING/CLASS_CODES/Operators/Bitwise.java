16340ef7a8f0ff3a1b9bd42356c8c47f0ff2b72b
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:21:38 +0530

    Bitwise operators

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Operators/Bitwise.java
@@ -0,0 +1,25 @@
+
+
+// Bitwise operators ( & , | , ^ , << , >> , >>>)
+//
+//
+class Operators{
+
+	public static void main(String[] args){
+	
+		int x = 5; 
+		int y = 7;
+
+		// bitwise ( & )
+		System.out.println(x & y);// 5
+
+		// Bitwise ( | )
+		System.out.println(x | y);// 7
+					  //
+		// Bitwise ( << )
+		System.out.println(x << 2);// it shifts the no of position to left side ( left shift)
+
+		// Bitwise ( >> )
+		System.out.println(y >> 2);// it shift the no of bits to right side ( right shift)
+	}
+}
