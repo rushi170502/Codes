a9bef11e39e47bbe92ff871342edc0ec31e485b4
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-06 00:43:58 +0530

    Write a program to print the first ten, 3 digit number

---
+++ C_PROGRAMMING/C_PROGRAMS/Statements/1_Basics/p3.c
@@ -0,0 +1,15 @@
+//Write a program to print first ten 3 digit numbers
+
+#include<stdio.h>
+void main(){
+
+	int i;
+	
+	printf("\n");
+
+	for(i = 200; i <= 209; i++){
+		printf(" %d ", i);
+	}
+	
+	printf("\n\n");
+}
