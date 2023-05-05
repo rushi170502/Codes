fbadaf316943f7df32271a29587bb789a3e34177
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-06 00:10:35 +0530

    find the character at a specific index

---
+++ JAVA_PROGRAMMING/CLASS_CODES/String/String_Methods/Predefined-Methods/charAt.java
@@ -0,0 +1,14 @@
+
+
+class MethodDemo{
+    public static void main(String[] args) {
+       
+        String str = "Rushi Birari";
+
+        System.out.println("character at 0 : "+str.charAt(0));
+        System.out.println("character at 1 : "+str.charAt(1));  
+        System.out.println("character at 2 : "+str.charAt(2));
+        System.out.println("character at 3 : "+str.charAt(3));
+    }
+
+}
