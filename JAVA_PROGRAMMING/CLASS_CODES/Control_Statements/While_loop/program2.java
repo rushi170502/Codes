2208f7b4f2bd53bc4821e2e1eb877d30b0e9faa2
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:53:12 +0530

    WAP to print given string for number of times by using while loop

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Control_Statements/While_loop/program2.java
@@ -0,0 +1,18 @@
+
+// program to print name n times
+//
+
+class Demo{
+
+	public static void main (String[] args){
+
+		int i  = 1;
+
+		while(i <= 5){
+
+			System.out.println("Rushi...");
+			i++;
+		}
+
+	}
+}
