d805c98604da194b9b7cc5922370b602640d8dc8
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:28:16 +0530

    WAP to print all even numbers to N number using for loop

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Control_Statements/For_loop/program2.java
@@ -0,0 +1,18 @@
+
+// WAP to print the odd numbers between 1 to N
+//
+
+class Demo{
+
+	public static void main(String[] args){
+
+		int num = 35;
+
+		for(int i = 1; i <= num; i++){
+
+			if(i % 2 == 0)
+				System.out.println(i);
+
+		}
+	}
+}
