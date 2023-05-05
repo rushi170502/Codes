ae9051b629fd4651eebb06d9e20b6f755025c749
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:44:43 +0530

    WAP to use of switch statement for the given input.

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Control_Statements/Switch/program1.java
@@ -0,0 +1,26 @@
+class switchDemo{
+    public static void main(String[] args) {
+        int x = 6;
+        switch(x){
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            default:
+                System.out.println("No Match..");
+        }
+    }
+
+
+}
