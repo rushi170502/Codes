1dbdb546ed7d2af0cbc31aab214e96de0dbcb08a
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 21:42:42 +0530

    concatenation string and check hashCode

---
+++ JAVA_PROGRAMMING/CLASS_CODES/String/string/program7.java
@@ -0,0 +1,19 @@
+package string;
+
+
+class Demo{
+    public static void main(String[] args) {
+        String str1 = "Rushi";
+        String str2 = "Birari";
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+        String str3 = str2.concat(str1);
+        System.out.println(str3);
+        System.out.println(str1);
+        System.out.println(str2);
+    
+        
+    }
+}
