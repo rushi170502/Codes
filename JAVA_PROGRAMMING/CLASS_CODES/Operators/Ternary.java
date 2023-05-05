882e4db61c62622cd1a882ab1a1274b83cd0e026
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:23:14 +0530

    Ternary  operators

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Operators/Ternary.java
@@ -0,0 +1,16 @@
+
+// Ternary operator...( ?  :)
+//
+class Operators{
+
+	public static void main(String[] args){
+
+		int x = 10;
+		int y = 20;
+
+		System.out.println((x > y) ? x : y);// 20
+
+		System.out.println((x < y) ? x : y);// 10
+
+	}
+}
