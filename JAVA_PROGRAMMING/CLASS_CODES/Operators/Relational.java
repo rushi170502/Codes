33a04e05c1412927091246ff72d885adc5ca3b9a
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:22:39 +0530

    Relational operators

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Operators/Relational.java
@@ -0,0 +1,19 @@
+// Relational operators...
+
+class Operators{
+
+	public static void main(String[] args){
+		
+		int x = 10;
+		int y = 20;
+
+		System.out.println(x > y);// false
+		System.out.println(x < y);// true
+		System.out.println(x >= y);// false
+		System.out.println(x <= y);// true
+		System.out.println(x == y);// false
+		System.out.println(x != y);// true
+		
+	}
+
+}
