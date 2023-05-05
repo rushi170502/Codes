1f7cdfcce05e198a63131d71d1fc18eb0d0d8827
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:54:13 +0530

    WAP to print all even numbers from n number. by using while loop

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Control_Statements/While_loop/program4.java
@@ -0,0 +1,20 @@
+
+// WAP to print all even numbers from n number.
+
+
+class Demo{
+
+	public static void main(String[] args){
+
+		int num =20;
+		int i = 1;
+
+		while(i <= num){
+			if(i % 2 == 0)
+				System.out.println(i);
+
+			i++;
+		}
+	}
+
+}
