a229e0bde5ccfcad4cfb2f10fbd1748c2ffc3901
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:36:25 +0530

    WAP to check maximum number

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Control_Statements/If_&_ifelse/max_num.java
@@ -0,0 +1,25 @@
+
+// WAP to which no. is greater
+//
+ class Demo{
+
+	 public static void main(String[] args){
+	 
+		 int x = 10;
+		 int y = 20;
+
+		 if( x > y)
+			 System.out.println(x + " is greater");
+		 else
+			 System.out.println(y + " is greater");
+
+
+		 int a =50; 
+		 int b = 10;
+
+		 if( a > b)
+			 System.out.println(a + " is greater");
+		 else
+			 System.out.println(b + " is greater");
+	 }
+ }
