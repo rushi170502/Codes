d45efef78e1ce99be506d2e5ec26f9ce1102f85b
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:34:36 +0530

    WAP to check number is divisble by

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Control_Statements/If_&_ifelse/divisible_check.java
@@ -0,0 +1,17 @@
+
+// WAP to find the given number is divisible by  4 or not
+//
+
+class Demo{
+
+
+	public static void main(String[] args){
+
+		int num = 12;
+
+		if( num % 4 == 0)
+			System.out.println(num+ " is divisble by 4");
+		else
+			System.out.println(num+ " is not divisibe by 4 ");
+	}
+}
