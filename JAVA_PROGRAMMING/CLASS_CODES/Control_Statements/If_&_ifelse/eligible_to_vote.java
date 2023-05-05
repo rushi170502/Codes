3fb7519a86b767542596585c66c771c6e8cce215
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:36:02 +0530

    WAP to check person is eligible for voting or not

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Control_Statements/If_&_ifelse/eligible_to_vote.java
@@ -0,0 +1,16 @@
+
+// WAP to check the person is eligible to vote or not..if yes then print eligible
+// hard coded
+
+class Demo{
+
+	public static void main(String[] args){
+		
+		int age = 25;
+
+		if( age > 18)
+		System.out.println("Person is eligible to vote.");	
+
+	}
+
+}
