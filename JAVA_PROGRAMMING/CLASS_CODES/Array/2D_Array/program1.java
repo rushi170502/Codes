9e2c0662433afc71caa53820f2807db0fb374722
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 21:25:01 +0530

    WAP to initialised Intger array and check the default values

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Array/2D_Array/program1.java
@@ -0,0 +1,18 @@
+
+class ArrayDemo {
+    public static void main(String[] args) {
+        int arr[][] = new int[3][3];
+        System.out.println(arr[0][0]);
+        System.out.println(arr[0][1]);
+        System.out.println(arr[0][2]);
+        System.out.println(arr[1][0]);
+        System.out.println(arr[1][1]);
+        System.out.println(arr[1][2]);
+        System.out.println(arr[2][0]);
+        System.out.println(arr[2][1]);
+        System.out.println(arr[2][2]);
+        
+  
+    }
+    
+}
