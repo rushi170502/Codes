33c95e28cb3f9da64dd8fbe7ccb1cff14bc38731
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 21:29:10 +0530

    trial &  errors on 2D array

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Array/2D_Array/program8.java
@@ -0,0 +1,18 @@
+ 
+ // trial and error
+
+ class ArrayDemo{
+    public static void main(String[] args) {
+        
+        int arr[][] = { {},{12,23},{} };
+        int arr1[][] = new int[2][];
+
+        System.out.println(arr.length);
+        System.out.println(arr[0].length);
+
+        System.out.println(arr1.length);
+        //System.out.println(arr1[0].length);
+
+        System.out.println(arr[1][1]);
+    }
+ }
