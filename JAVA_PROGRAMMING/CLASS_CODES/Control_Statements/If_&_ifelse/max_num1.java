7459e27cb0ba1a76da151494ac7ed69d8aef8aa9
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-05 20:36:47 +0530

    WAP to check maximum number

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Control_Statements/If_&_ifelse/max_num1.java
@@ -0,0 +1,21 @@
+
+// WAP to find which element is greater 
+// assume numbers are also same
+//
+
+class Demo{
+
+	public static void main(String[] args){
+
+		int a = 10;
+		int b = 11;
+
+		if( a == b)
+			System.out.println(a+ " & " +b+ " are same");
+		else if(a > b)
+			System.out.println(a+ " is greater ");
+		else
+			System.out.println(b+ " is greater");
+	}
+
+}
