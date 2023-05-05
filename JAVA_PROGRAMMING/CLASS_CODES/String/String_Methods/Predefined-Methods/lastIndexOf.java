141ce72fb5b8e291d25cefccd4cd9ee37739895a
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-06 00:20:31 +0530

    return the index of last instance of given character in a string

---
+++ JAVA_PROGRAMMING/CLASS_CODES/String/String_Methods/Predefined-Methods/lastIndexOf.java
@@ -0,0 +1,15 @@
+
+// lastIndexOf() method :
+import java.util.*;
+
+class MethodDemo{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the string");
+    String str1 = sc.nextLine();
+   
+    // method uses
+    System.out.println(" last instance index is : "+str1.lastIndexOf('s', 4));
+
+    }
+}
