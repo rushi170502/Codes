cb2bd737452ed8479b56b2a2fa4d1599ccf77892
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-06 00:16:23 +0530

    check the content of two strings

---
+++ JAVA_PROGRAMMING/CLASS_CODES/String/String_Methods/Predefined-Methods/equals.java
@@ -0,0 +1,16 @@
+import  java.util.*;
+
+class MethodDemo{
+    public static void main(String[] args) {
+        // taking from user 
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string 1 : ");
+        String str1 = sc.next();
+        System.out.println("Enter string 2 : ");
+        String str2 = sc.next();
+
+        System.out.println(str1.equals(str2));
+        sc.close();
+    }
+}
